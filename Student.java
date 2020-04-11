d4dc21c975df5ab99085daec2febac0d0b15cb1b
Vladimir Stoilov <vladimir.stoilov@students.finki.ukim.mk>
2020-04-11 16:09:36 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -8,15 +8,47 @@ class Student {
 	
 	List<Integer> grades = new ArrayList();
 
-	//TODO constructor
+	public Student() {
 
-	//TODO seters & getters
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String value) {
+		index = value;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String value) {
+		firstName = value;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String value) {
+		lastName = value;
+	}
 
 	public double getAverage() {
-		//TODO
+		double sum = 0;
+		for (int grade : grades) {
+			sum += grade;
+		}
+
+		if (sum == 0)
+			return 0;
+
+		return sum / grades.size();
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		return grades.size() * 6;
 	}
 }
