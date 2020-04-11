21b0f505b65ab816b883fa410ebad39f379ceb86
Vladimir Stoilov <vladimir.stoilov@students.finki.ukim.mk>
2020-04-11 16:02:56 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -1,7 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
+	
+	List<Integer> grades = new ArrayList();
 
 	//TODO constructor
 
